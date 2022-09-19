6fac87c45ba652612674883f6ece31b76d397028
azure928 <azure6.dev@gmail.com>
2022-09-19 20:01:44 +0900

    Modify: 보스레이드 상태 조회 api 수정 - status가 진행 중일 경우 입장시간으로부터 3분 이상 지났다면 canEnter = true, 3분이 지나지 않았다면 false가 되도록 수정

--- src/bossRaid/bossRaidRepository.js
+++ src/bossRaid/bossRaidRepository.js
@@ -1,14 +1,10 @@
 const db = require('../../database/models/index');
 const Boss_raid_history = db.boss_raid_history;
-const SQ = require('sequelize');
-const Sequelize = SQ.Sequelize;
-const sequelize = require('sequelize');
-const Op = sequelize.Op;
 
-/**  보스레이드 상태 조회  */
+/**  보스레이드 상태 조회(보스레이드 현재 상태 응답) */
 async function readBossRaidStatus() {
   return await Boss_raid_history.findAll({
-    attributes: ['user_id', 'enter_time', 'end_time', 'status'],
+    attributes: ['user_id', 'enter_time', 'status'],
     order: [['createdAt', 'DESC']],
     limit: 1,
   });
--- src/bossRaid/bossRaidService.js
+++ src/bossRaid/bossRaidService.js
@@ -1,27 +1,41 @@
 const bossRaidRepository = require('./bossRaidRepository');
+const moment = require('moment');
 
 // 보스레이드 상태 조회
 async function readBossRaidStatus() {
   const bossRaidStatus = await bossRaidRepository.readBossRaidStatus();
   let canEnter;
-  let enteredUserId;
+  const enter_time = moment(bossRaidStatus[0].enter_time);
 
+  //console.log('enter_time', enter_time);
   //console.log('bossRaidStatus!!', bossRaidStatus[0].status);
 
   if (
     bossRaidStatus[0].status == '성공' ||
     bossRaidStatus[0].status == '실패'
   ) {
-    (canEnter = true), (enteredUserId = bossRaidStatus[0].user_id);
+    canEnter = true;
   }
 
   if (bossRaidStatus[0].status == '진행중') {
-    (canEnter = false), (enteredUserId = bossRaidStatus[0].user_id);
+    const today = moment();
+    //console.log('today', today);
+
+    const timeDiff = moment.duration(today.diff(enter_time)).asMinutes();
+    console.log('분 차이: ', timeDiff);
+
+    if (timeDiff >= 3) {
+      canEnter = true;
+    }
+
+    if (timeDiff < 3) {
+      canEnter = false;
+    }
   }
 
   const result = {
     canEnter: canEnter,
-    enteredUserId: enteredUserId,
+    enteredUserId: bossRaidStatus[0].user_id,
   };
 
   return result;
