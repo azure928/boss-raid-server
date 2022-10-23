1499c5d7aba920ba45700ead75af6990c67d17af
azure928 <azure6.dev@gmail.com>
2022-10-24 00:07:17 +0900

    Modify: server.js 를 app.js, server.js 로 분리

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const routes = require('./src/indexRouter');
+const { sequelize } = require('./database/models/index');
+
+//* DB 연결
+sequelize
+  .sync({ force: false }) // true 로 설정하면 서버 실행마다 테이블 재생성
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+app.use(routes);
+
+app.get('/ping', (req, res) => {
+  return res.status(200).json({ message: 'pong' });
+});
+
+// 등록되지 않은 라우터로 요청이 들어왔을 때 처리
+app.use((req, res, next) => {
+  res.status(404).json({ error: `${req.url} 라우터가 없습니다.` });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,31 +1,8 @@
-const http = require('http');
-const morgan = require('morgan');
-const express = require('express');
-const dotenv = require('dotenv');
-const routes = require('./src/indexRouter');
-const { sequelize } = require('./database/models/index');
-
-dotenv.config();
-
-//* DB 연결
-sequelize
-  .sync({ force: false }) // true 로 설정하면 서버 실행마다 테이블 재생성
-  .then(() => {
-    console.log('데이터베이스 연결 성공');
-  })
-  .catch(err => {
-    console.error(err);
-  });
-
-const app = express();
-
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(routes);
-
-const server = http.createServer(app);
+const app = require('./app');
 const PORT = process.env.PORT || 10010;
+//const logger = require('./logger');
 
-server.listen(PORT, () => {
-  console.log(`server start PORT:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server start : http://localhost:${PORT}/`);
+  //logger.info(`server start : http://localhost:${PORT}/`);
 });
